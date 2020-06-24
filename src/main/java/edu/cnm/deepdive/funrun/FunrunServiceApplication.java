14bc1fefc019e211dfe88cf701f539b679710299
Anastasia Hristian <an.hristian@gmail.com>
2020-06-24 14:12:40 -0600

    Initial commit.

---
+++ src/main/java/edu/cnm/deepdive/funrun/FunrunServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.funrun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunrunServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FunrunServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/edu/cnm/deepdive/funrun/FunrunServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.funrun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunrunServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
