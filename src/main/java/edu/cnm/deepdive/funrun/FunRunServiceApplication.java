578dacb3e70afbffc13ecc5b04c7e7fad0a3bf15
Christie Ryan <cryan11@cnm.edu>
2020-06-24 15:00:13 -0600

    Refactored name

--- src/main/java/edu/cnm/deepdive/funrun/FunRunServiceApplication.java
+++ src/main/java/edu/cnm/deepdive/funrun/FunRunServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FunrunServiceApplication {
+public class FunRunServiceApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(FunrunServiceApplication.class, args);
+    SpringApplication.run(FunRunServiceApplication.class, args);
   }
 
 }
